9f9840f5174cf56cee1767ef1eec51d422d00508
peterkyle28 <143087590+peterkyle28@users.noreply.github.com>
2023-11-19 18:41:29 +0800

    initial change

---
+++ info.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickName:'Peter',
+      name: {
+        firstName:'PETER KYLE',
+        lastName:'GINGO',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'inday',
+      name: {
+        firstName:'SHEILA ',
+        lastName:'DELMONTER',
+      },
+      course:'EDUCATION',
+      year:'3',
+    },
+    {
+      nickName:'Batotoy',
+      name: {
+        firstName:'ALEXANDER',
+        lastName:'GINGO',
+      },
+      course:'CRIM',
+      year:'4',
+    },
+    {
+      nickName:'Lene',
+      name: {
+        firstName:'CELENE',
+        lastName:'GINGO',
+      },
+      course:'ENGINEERING',
+      year:'3',
+    },
+    {
+      nickName:'Lai',
+      name: {
+        firstName:'Allysa',
+        lastName:'Tabig',
+      },
+      course:'CRIMINOLOGY',
+      year:'3',
+    },
+  ];
